fd78d1f5e894559654f479a8f9afe320b97a4262
Radek Holubowski <radekh8787@gmail.com>
2022-02-18 18:05:59 +0000

    Save index.ts file

---
+++ pages/api/create.ts
@@ -0,0 +1,23 @@
+import { prisma } from "../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { title, content } = req.body;
+
+  try {
+    await prisma.note.create({
+      data: {
+        title,
+        content,
+      },
+    });
+    res.status(200).json({ message: "Note Created" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "Something went wrong, please try again later" });
+  }
+}
